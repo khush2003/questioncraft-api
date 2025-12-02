1eb2ab17588a50fe18a06f5cd54460f87f3f9be9
Khush Agarwal <98137724+khush2003@users.noreply.github.com>
2025-12-02 23:36:01 +0700

    Update port

--- api/index.py
+++ api/index.py
@@ -665,4 +665,4 @@ async def get_topics(input: str):
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
